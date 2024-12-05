ec7ed6f8b7765862dcfeb7deb53b9e4e77d2ae39
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2024-12-05 14:46:22 +0100

    fix: translations

--- pages/tour/[id].vue
+++ pages/tour/[id].vue
@@ -184,31 +184,102 @@ function onMicrioError() {
         </template>
       </Navbar>
       <div
-        class="flex h-[calc(100%-80px)] w-full items-center justify-between gap-16 pb-20 pl-16 pr-6 z-50"
+        class="z-50 grid h-[calc(100%-80px)] w-full grid-cols-2 gap-8 p-10 pb-20"
       >
-        <div class="flex flex-col gap-16">
-          <article class="flex flex-col gap-9">
-            <h1 class="text-5xl font-medium">Objavuj detail diela cez audio</h1>
-            <span class="text-2xl"
-              >Dozveď sa viac o diele v tejto interaktívnej prehliadke a vypočuj
-              si jeho príbeh.</span
-            >
-          </article>
-          <div class="flex flex-col gap-6">
+        <div class="flex flex-col">
+          <span class="text-3xl pb-8 font-bold">{{$t('Vytvorili') }}
+          </span>
+          <div class="grid grid-cols-2 gap-4">
+            <div class="flex flex-col gap-8">
+              <span class="text-2xl">{{ $t('Slovenská Národná Galéria')}}</span>
+              <div class="flex flex-col text-xl">
+                <div class="font-bold italic">{{ $t('Vedenie projektu')}}</div>
+                <div>Michal Čudrnák</div>
+              </div>
+              <div class="flex flex-col text-xl">
+                <div class="font-bold italic">{{ $t('Obsah')}}</div>
+                <div>Lukáš Štepánovský</div>
+              </div>
+              <div class="flex flex-col text-xl">
+                <div class="font-bold italic">{{ $t('Dizajn')}}</div>
+                <div>Filip Riusl</div>
+              </div>
+              <div class="flex flex-col text-xl">
+                <div class="font-bold italic">{{ $t('Vývoj')}}</div>
+                <div>František Michal Sebestyén</div>
+              </div>
+              <div class="flex flex-col text-xl">
+                <div class="font-bold italic">{{ $t('Odborná spolupráca')}}</div>
+                <div>
+                  Jana Švantnerová, Martin Čičo, Helena Gahérová, Attila Kovács,
+                  Eva Hasalová
+                </div>
+              </div>
+              <div class="flex flex-col text-xl">
+                <div class="font-bold italic">{{ $t('Preklad')}}</div>
+                <div>Isabel Stainsby</div>
+              </div>
+            </div>
+            <div class="grid grid-cols-2">
+              <div class="flex flex-col gap-8">
+                <span class="text-2xl">Storylab Audio</span>
+                <div class="flex flex-col text-xl">
+                  <div class="font-bold italic">{{ $t('Produkcia, réžia, scenár')}}</div>
+                  <div>Damian Machaj</div>
+                </div>
+                <div class="flex flex-col text-xl">
+                  <div class="font-bold italic">{{ $t('Ženský hlas (SK)')}}</div>
+                  <div>Miroslava Frankovská</div>
+                </div>
+                <div class="flex flex-col text-xl">
+                  <div class="font-bold italic">{{ $t('Mužský hlas (SK)')}}</div>
+                  <div>Martin Mňahončák</div>
+                </div>
+                <div class="flex flex-col text-xl">
+                  <div class="font-bold italic">{{ $t('Ženský hlas (EN)')}}</div>
+                  <div>Jenn Kirk</div>
+                </div>
+                <div class="flex flex-col text-xl">
+                  <div class="font-bold italic">{{ $t('Mužský hlas (EN)')}}</div>
+                  <div>Mike Ackerman</div>
+                </div>
+                <div class="flex flex-col text-xl">
+                  <div class="font-bold italic">{{ $t('Mixing, sound design')}}</div>
+                  <div>Petr Čechák</div>
+                </div>
+                <div class="flex flex-col text-xl">
+                  <div class="font-bold italic">{{ $t('Jazykové korekcie (EN)')}}</div>
+                  <div>Brian Vondrak</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <span class="font-bold text-3xl pb-8">{{ $t('Použité diela') }}</span>
+          <ul class="text-lg flex flex-col gap-3">
+            <li>{{ $t('Neznámy maliar: Esterházyovský štvorpohreb v Trnave. Po roku 1652, Esterházy Privatstiftung, Eisenstadt, Inv. č. B 467, hrad Forchtenstein. ')}}</li>
+            <li>{{ $t('Neznámy maliar: Olejomaľba inscenovaného žánrového výjavu Sultán v Benátkach, podľa predlôh z knihy Nicolasa de Nicolay. Záver 17. storočia, Zbierka SNM – Múzeum Bojnice, Inv. č. XI-2760.')}}</li>
+            <li>{{ $t('Nicolas de Nicolay: Les quatre premiers livres des Navigations et Peregrinations Orientales. 1568, Victoria and Albert Museum.')}}</li>
+            <li>{{ $t('Elias Widemann: Ladislav Esterházy. 1649.')}}</li>
+            <li>{{ $t('Neznámy maliar: Posmrtná podobizeň grófa Ladislava Esterházyho. okolo 1652. Esterhazy Privatstiftung, hrad Forchtenstein.')}}</li>
+            <li>{{ $t('Košeľa Ladislava Esterházyho. Polovica 17, storočia, Múzeum úžitkového umenia v Budapešti.')}}</li>
+            <li>{{ $t('Johann (Hans) Rudolf Miller – Moritz Lang: Pohrebný sprievod štyroch padlých Esterháziovcov: Ladislava, Františka, Tomáša a Gašpara v bitke pri Vozokanoch v roku 1652 – Vera delineatio pompae finebris Illmi Com. Ladislai Eszterhazi et sociorum cognatorum habitae Tyrnaviae in Ungaria 1652. 26. Novembris. 1653, Lept. ELTE Egyetemi Könyvtár és Levéltár, Budapešť.')}}</li>
+            <li>{{ $t('Pohrebná zástava Ladislava Esterházyho. 1652, výšivka s aplikáciami, Burg Forchtenstein.')}}</li>
+            <li>{{ $t('Johann (Hans) Rudolf Miller – Moritz Lang : Castrum doloris pri pohrebe štyroch padlých, v bitke pri Vozokanoch padlých Esterházyovcov, v jezuitskom kostole v Trnave. 1652 – 1653, lept. Magyar Nemzeti Múzeum, Budapešť.')}}</li>
+            <li>{{ $t('Lucas Schnitzer: Gróf Adam Forgáč. 1630 – 1700, Galéria mesta Bratislavy.')}}</li>
+            <li>{{ $t('Jozef Jägr: Veduta Trnavy. 40. roky 18. storočia, Štátny archív v Trnave.')}}</li>
+            <li>{{ $t('Johann Daniel Ther Portten: Fasáda trnavského univerzitného kostola sv. Jána Krstiteľa. 1694.')}}</li>
+          </ul>
+          <div class="flex w-full items-center justify-center mt-auto gap-6 [&>*]:w-full">
             <button
-              class="w-full rounded-xl bg-blue-ribbon-600 py-3 text-lg text-white"
+              class="rounded-xl bg-blue-ribbon-600 py-3 text-lg text-white"
               @click="closeHint"
             >
               {{ $t("Pokračuj") }}
             </button>
             <div id="restart-button" />
           </div>
-        </div>
-        <div class="flex h-full w-full items-center justify-center">
-          <img
-            :src="TOURS[id].thumbnailSrc"
-            class="max-h-full max-w-full rounded-3xl object-contain"
-          />
         </div>
       </div>
     </div>
@@ -243,7 +314,11 @@ function onMicrioError() {
             v-if="micrio?.tour && micrio?.marker"
             class="pointer-events-none absolute inset-0 flex items-end justify-end p-4"
           >
-            <Annotation @onPrevClick="onPrevClick" @onNextClick="onNextClick" v-show="overlay !== 'hint'">
+            <Annotation
+              @onPrevClick="onPrevClick"
+              @onNextClick="onNextClick"
+              v-show="overlay !== 'hint'"
+            >
               <template #header>
                 <span>{{ (micrio.tour.currentStep ?? 0) + 1 }}. </span>
                 <span>{{ micrio.marker.title }}</span>
